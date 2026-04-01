cdfef7add2e772d93a7d75021109a0a7d64bc13b
dk <kudin.dima@gmail.com>
2026-04-01 13:25:27 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'The Barber House | Professional Grooming in Prague',
+  description: 'The Barber House offers expert grooming, haircuts, and styling for men in Prague. Book your appointment at our professional barber shop on Libušská 319.',
+  keywords: ["barber shop Prague, pánské holičství Praha, The Barber House, haircut Praha, men's grooming"],
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+          
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
